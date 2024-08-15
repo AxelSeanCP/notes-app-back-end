228ecd787ec1f1a5083b52a49ea51a918283691b
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-08-15 17:09:11 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -24,6 +24,11 @@ const collaborations = require("./api/collaborations");
 const CollaborationsService = require("./services/postgres/CollaborationsService");
 const CollaborationsValidator = require("./validator/collaborations");
 
+// exports
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
@@ -96,6 +101,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
