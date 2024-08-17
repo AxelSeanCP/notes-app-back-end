1bd7743efd65e1878689b20390d162e88dcfd809
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-08-17 21:27:36 +0700

    S3 storage implemented

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ const ExportsValidator = require("./validator/exports");
 
 // uploads
 const uploads = require("./api/uploads");
-const StorageService = require("./services/storage/StorageService");
+const StorageService = require("./services/S3/StorageService");
 const UploadsValidator = require("./validator/uploads");
 
 const ClientError = require("./exceptions/ClientError");
@@ -43,9 +43,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(
-    path.resolve(__dirname, "api/uploads/file/images")
-  );
+  const storageService = new StorageService();
 
   const server = Hapi.server({
     port: process.env.PORT,
